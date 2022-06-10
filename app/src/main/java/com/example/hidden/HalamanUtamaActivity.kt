0223707014c202d8bca1496cabe879e3fbd9907d
Nicholas3413 <nicklin42@gmail.com>
2022-06-10 21:06:00 +0700

    Third Commit Hidden, minor edit

--- app/src/main/java/com/example/hidden/HalamanUtamaActivity.kt
+++ app/src/main/java/com/example/hidden/HalamanUtamaActivity.kt
@@ -3,11 +3,13 @@ package com.example.hidden
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_halaman_utama.*
 
 class HalamanUtamaActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_utama)
@@ -18,6 +20,15 @@ class HalamanUtamaActivity : AppCompatActivity() {
         }
         btnRegisterUtama.setOnClickListener {
             val intent = Intent(this, RegAkunPemilikActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    public override fun onStart() {
+        super.onStart()
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            intent= Intent(this,HomePemilikActivity::class.java)
             startActivity(intent)
         }
     }
